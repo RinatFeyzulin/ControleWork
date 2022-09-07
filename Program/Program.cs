c427be4ec1241b231b4934ff1722dae5715229f1
«Rinat» <rtatarin949@gmail.com>
2022-09-07 20:53:21 +0300

    набросок программы

---
+++ Program/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите массив строк");
+string consolеRead = Console.ReadLine();
+
+string [] overwritingArray(string str)
+{
+    return ;
+}
+
+void Print(string[]arr)
+{
+}
+
+if(consolеRead == "")Console.WriteLine("Пустая строка");
+else Print(overwritingArray(consolеRead));
